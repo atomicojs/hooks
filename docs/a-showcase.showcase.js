a1bab2d62b0d3962f069450860f4d680862e7e08
UpperCod <mtrujillo@outlook.cl>
2020-04-01 14:05:34 -0300

    build

--- docs/a-showcase.showcase.js
+++ docs/a-showcase.showcase.js
@@ -1,21 +1,2 @@
-import { b as createElement } from './chunks/eaceffd6.js';
-export { r as render } from './chunks/eaceffd6.js';
-
-var aShowcase_showcase = [{
-  label: "Ejemplo 1",
-
-  render() {
-    return createElement("h1", null, "1");
-  }
-
-}, {
-  label: "Ejemplo 2",
-
-  render() {
-    return createElement("h1", null, "2");
-  }
-
-}];
-
-export default aShowcase_showcase;
+import{b as e}from"./chunks/eaceffd6.js";export{r as render}from"./chunks/eaceffd6.js";var l=[{label:"Ejemplo 1",render:()=>e("h1",null,"1")},{label:"Ejemplo 2",render:()=>e("h1",null,"2")}];export default l;
 //# sourceMappingURL=a-showcase.showcase.js.map
