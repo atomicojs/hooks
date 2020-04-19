a2af001be5620f8bce5908a87dea62d8a55f0260
UpperCod <mtrujillo@outlook.cl>
2020-04-19 15:07:46 -0400

    updates dispatched by hook are improved, use-media-resize

--- src/hooks/use-media-resize/use-media-resize.js
+++ src/hooks/use-media-resize/use-media-resize.js
@@ -1,9 +1,60 @@
 import { useEffect, useState } from "atomico";
 
+export function useMediaResize(value, ref = {}) {
+  let sizes = getSizes(value); // The computation process is light in cost vs useMemo
+
+  let [state, setState] = useState(() => [null, ref]);
+
+  state[2] = sizes;
+
+  useEffect(() => {
+    let [, ref] = state;
+
+    if (!ref.listeners) {
+      let listeners = [];
+      let observer = new ResizeObserver(
+        ([
+          {
+            contentRect: { width },
+          },
+        ]) => {
+          listeners.forEach((fn) => fn(width));
+        }
+      );
+      ref.listeners = listeners;
+      observer.observe(ref.current);
+    }
+
+    let listener = (width) => {
+      let sizes = state[2];
+      if (
+        !sizes.cases.some(([value, px]) => {
+          if (px >= width) {
+            setState(updateState(value));
+            return true;
+          }
+        })
+      ) {
+        setState(updateState(sizes.default || value));
+      }
+    };
+
+    ref.listeners.push(listener);
+
+    return () => ref.listeners.splice(ref.listeners.indexOf(listener) >>> 0, 1);
+  }, []);
+  return state;
+}
+
+let updateState = (nextValue) => (currentState) => {
+  let [value, ...state] = currentState;
+  return nextValue == value ? currentState : [nextValue, ...state];
+};
+
 function getSizes(value) {
   let sizes = { cases: [] };
 
-  value.split(/ *, */).forEach(value => {
+  value.split(/ *, */).forEach((value) => {
     let size = value.match(/([^\s]+) +(\d+)px/);
     if (size) {
       let [, value, number] = size;
@@ -16,43 +67,4 @@ function getSizes(value) {
   sizes.cases.sort(([, a], [, b]) => (a > b ? 1 : -1));
 
   return sizes;
-}
-
-export function useMediaResize(value) {
-  let sizes = getSizes(value);
-
-  let [state, setState] = useState(() => [null, {}]);
-
-  state[2] = sizes;
-
-  useEffect(() => {
-    let observer = new ResizeObserver(
-      ([
-        {
-          contentRect: { width }
-        }
-      ]) => {
-        update(width);
-      }
-    );
-
-    observer.observe(state[1].current);
-
-    function update(width) {
-      let sizes = state[2];
-      if (
-        !sizes.cases.some(([value, px]) => {
-          if (px >= width) {
-            setState(([, ...state]) => [value, ...state]);
-            return true;
-          }
-        })
-      ) {
-        setState(([value, ...state]) => [sizes.default || value, ...state]);
-      }
-    }
-    return () => resizeObserver.unobserve(state.ref.current);
-  }, []);
-
-  return state;
 }
