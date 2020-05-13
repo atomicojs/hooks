61218371a306a0026ef8cfe37caf668a6c6d72a1
UpperCod <mtrujillo@outlook.cl>
2020-05-13 12:57:34 -0400

    build:doc

--- docs/a-showcase.showcase.js
+++ docs/a-showcase.showcase.js
@@ -1,23 +1,2 @@
-import { p } from './chunks/61e0f1f4.js';
-export { d as render } from './chunks/61e0f1f4.js';
-
-var aShowcase_showcase = [{
-  label: "Ejemplo 1",
-
-  render() {
-    return p("host", null, p("h1", null, "1"));
-  }
-
-}, {
-  label: "Ejemplo 2",
-
-  render() {
-    return p("host", {
-      style: "background:black"
-    }, p("h1", null, "2"));
-  }
-
-}];
-
-export default aShowcase_showcase;
+import{p as e}from"./chunks/61e0f1f4.js";export{d as render}from"./chunks/61e0f1f4.js";var l=[{label:"Ejemplo 1",render:()=>e("host",null,e("h1",null,"1"))},{label:"Ejemplo 2",render:()=>e("host",{style:"background:black"},e("h1",null,"2"))}];export default l;
 //# sourceMappingURL=a-showcase.showcase.js.map
