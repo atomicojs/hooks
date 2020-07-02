17de6c451ca870b3e08fb54298a7219b503bf06e
UpperCod <mtrujillo@outlook.cl>
2020-07-02 17:20:53 -0400

    atomico version is updated

---
+++ docs/chunks/aec7f535.js
@@ -0,0 +1,2 @@
+import{u as t,a as r}from"./1fcfa7f3.js";function e(e,n,o=[]){const[a,u]=t(["unimport"]);return r(()=>{if(!n||!e)return;let t,r;return setTimeout(()=>!r&&!t&&u(t=>"loading"==t[0]?t:["loading"]),100),e().then(e=>u(n=>(r=!0,t?n:["done",e&&"default"in e?e.default:e]))).catch(r=>u(e=>t?e:["error",r])),()=>t=!0},[n,...o]),a}export{e as u};
+//# sourceMappingURL=aec7f535.js.map
