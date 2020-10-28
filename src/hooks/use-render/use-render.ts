ff1e2b4700a9bd2219b7cc19fdcf6cf660b3837f
UpperCod <mtrujillo@outlook.cl>
2020-10-28 20:41:46 -0300

    test is added to useRender

--- src/hooks/use-render/use-render.ts
+++ src/hooks/use-render/use-render.ts
@@ -1,10 +1,18 @@
-import { render, useHost, useMemo } from "atomico";
+import { h, render, useHost, useMemo } from "atomico";
 
 type Callback = () => any;
 
+const host = h("host");
+
+const fillHost = (vdom: any): any => {
+  if (vdom && typeof vdom == "object") {
+    vdom = vdom.type == "host" ? vdom : h("host", null, vdom);
+    return vdom;
+  }
+  return host;
+};
+
 export function useRender(callback: Callback, args?: any[]) {
   const host = useHost();
-  useMemo(() => {
-    render(callback(), host.current);
-  }, args);
+  useMemo(() => render(fillHost(callback()), host.current), args);
 }
