ad6597e1e1bd10ec6b10ba0f0f91fcb720d78c7e
UpperCod <mtrujillo@outlook.cl>
2021-03-17 20:17:52 -0300

    fix test

--- src/use-router/use-router.test.js
+++ src/use-router/use-router.test.js
@@ -4,12 +4,18 @@ import { useRoute } from "./use-router.js";
 
 it("useRouter", () => {
   const hooks = createHooks(() => {});
-
+  let render = 0;
   function load() {
-    const [result] = useRoute("/[...any]");
+    const result = useRoute("/[...any]");
 
-    expect(result).to.deep.equal({ any: "" });
+    if (render++) {
+      expect(result[0]).to.deep.equal({ any: "" });
+    } else {
+      expect(result).to.undefined;
+    }
   }
 
   hooks.load(load);
+
+  hooks.cleanEffects()();
 });
