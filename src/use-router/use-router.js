7fcf3e2055ed47a598c8c40c869ff93b9f0796a9
UpperCod <mtrujillo@outlook.cl>
2021-03-17 20:04:59 -0300

    Improves the behavior of a useRouter

--- src/use-router/use-router.js
+++ src/use-router/use-router.js
@@ -2,24 +2,38 @@ import { useState, useEffect } from "atomico";
 import { getPath, listener } from "./src/history.js";
 import { matches } from "./src/matches.js";
 export { redirect } from "./src/history.js";
+
+/**@type {InternalState} */
+const DefaultState = {};
+
 /**
  *
  * @param {import("./src/matches").Routes} routes
  */
 export function useRouter(routes) {
-  let lastPath = getPath();
-  const [state, setState] = useState(() => matches(routes, lastPath));
-  useEffect(
-    () =>
-      listener(() => {
-        const currentPath = getPath();
-        if (currentPath != lastPath) {
-          setState(matches(routes, (lastPath = currentPath)));
-        }
-      }),
-    []
-  );
-  return state;
+  const [state, setState] = useState(DefaultState);
+
+  useEffect(() => {
+    if (!routes) return;
+
+    const reduce = () => {
+      setState((current) => {
+        const path = getPath();
+        return current.path != path
+          ? {
+              path,
+              result: matches(routes, path),
+            }
+          : current;
+      });
+    };
+
+    reduce();
+
+    return listener(reduce);
+  }, [routes]);
+
+  return state.result;
 }
 
 /**
@@ -31,3 +45,9 @@ export function useRoute(path, callback = (param) => param) {
   const routes = { [path]: callback };
   return useRouter(routes);
 }
+
+/**
+ * @typedef {Object} InternalState
+ * @property {string} [path]
+ * @property {any} [result]
+ */
