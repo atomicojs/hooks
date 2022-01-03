d53e7da079336bf5e081332e685810bf1f52246a
UpperCod <mtrujillo@outlook.cl>
2022-01-03 00:11:09 -0300

    remove filter from useReflectEvent and export the reflectEvent function to be used outside the hook

--- src/use-reflect-event/use-reflect-event.js
+++ src/use-reflect-event/use-reflect-event.js
@@ -1,13 +1,24 @@
 import { useListener } from "../use-listener/use-listener";
 
 /**
+ * reflects an event to another node
+ * @param {Element} current
+ * @param {Event} event
+ */
+export const reflectEvent = (current, event) => {
+  if (!event.composedPath().includes(current)) {
+    event.preventDefault();
+    event.stopPropagation();
+    current.dispatchEvent(new event.constructor(event.type, event));
+  }
+};
+/**
  * This hook reflects an event and cancels its propagation
  * @param {string} type
  * @param {import("atomico").Ref<Element>} refFrom - event source reference
  * @param {import("atomico").Ref<Element>} refTo - event destination reference
- * @param {(ev:Event)=>boolean} [filter] - Defines if the event should continue the reflection
  */
-export function useReflectEvent(refFrom, refTo, type, filter) {
+export function useReflectEvent(refFrom, refTo, type) {
   useListener(
     refFrom,
     type,
@@ -17,12 +28,7 @@ export function useReflectEvent(refFrom, refTo, type, filter) {
     (event) => {
       const { current } = refTo;
       if (!current) return;
-      if (!event.composedPath().includes(current)) {
-        if (filter && !filter(event)) return;
-        event.preventDefault();
-        event.stopPropagation();
-        current.dispatchEvent(new event.constructor(event.type, event));
-      }
+      reflectEvent(current, event);
     }
   );
 }
