207fe5cb6078434bcb6b4f566f40bb778363b29d
UpperCod <mtrujillo@outlook.cl>
2022-03-06 17:53:57 -0300

    now the parameter for useCssLightDom is of type Sheets

--- src/use-css-light-dom/use-css-light-dom.js
+++ src/use-css-light-dom/use-css-light-dom.js
@@ -4,7 +4,7 @@ import { getRules } from "./src/utils.js";
 let ID = 0;
 /**
  * Create a style collector to apply once the render is finished
- * @param {import("atomico").Sheet} sheet
+ * @param {import("atomico").Sheets} sheet
  */
 export function useCssLightDom(sheet) {
   const host = useHost();
@@ -20,10 +20,16 @@ export function useCssLightDom(sheet) {
 
     current.appendChild(style);
 
-    getRules(
-      sheet,
-      current.localName + `[data-sheet="${current.dataset.sheet}"]`
-    ).forEach((rule, index) => style.sheet.insertRule(rule, index));
+    (Array.isArray(sheet) ? sheet.flat(100) : [sheet]).forEach(
+      (sheet) =>
+        sheet &&
+        getRules(
+          sheet,
+          current.localName + `[data-sheet="${current.dataset.sheet}"]`
+        ).forEach((rule) =>
+          style.sheet.insertRule(rule, style.sheet.cssRules.length)
+        )
+    );
 
     return () => style.remove();
   }, [sheet]);
