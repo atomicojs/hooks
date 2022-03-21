8c911b53f3b7857c0ffc483b406bea2c3e185183
UpperCod <mtrujillo@outlook.cl>
2022-03-21 00:04:45 -0300

    add the useHistory hook

---
+++ src/use-history/use-history.js
@@ -0,0 +1,14 @@
+/**
+ * save the first parameter in a list
+ * @template T
+ * @param {T} value
+ * @param {number} [maxLength]
+ * @returns {T[]}
+ */
+export function useHistory(value, maxLength = 100) {
+  const [state] = useState(() => ({ history: [] }));
+  if (state.history.at(-1) !== value) {
+    state.history = [...state.history, value].slice(maxLength * -1);
+  }
+  return state.history;
+}
