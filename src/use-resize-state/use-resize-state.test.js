45e2cfff6422d118e702372605e8b018b66dacab
UpperCod <mtrujillo@outlook.cl>
2022-04-25 13:28:53 -0400

    fix test for github actions

--- src/use-resize-state/use-resize-state.test.js
+++ src/use-resize-state/use-resize-state.test.js
@@ -3,10 +3,11 @@ import { setViewport } from "@web/test-runner-commands";
 import { createHooks } from "atomico/test-hooks";
 import { useResizeState } from "./use-resize-state.js";
 
-it("useResponsiveState <= 320px", (done) => {
+it("useResponsiveState <= 320px", async () => {
   const current = document.createElement("div");
   const ref = { current };
   let step = 0;
+  const results = [];
 
   document.body.appendChild(current);
 
@@ -14,25 +15,18 @@ it("useResponsiveState <= 320px", (done) => {
 
   const load = () => {
     const value = useResizeState(ref, "no, yes 320px");
-    switch (step++) {
-      case 0:
-        expect(value).to.undefined;
-        break;
-      case 1:
-        expect(value).to.equal("yes");
-        break;
-      case 2:
-        expect(value).to.equal("no");
-        done();
-        break;
-    }
+    results.push(value);
   };
 
   hooks.load(load);
 
   hooks.cleanEffects()();
 
-  setViewport({ width: 360, height: 640 }).then(() => {
-    setViewport({ width: 200, height: 640 });
-  });
+  await setViewport({ width: 360, height: 640 });
+
+  await setViewport({ width: 200, height: 640 });
+
+  await new Promise((resolve) => setTimeout(resolve, 50));
+
+  expect(results).to.deep.equal([undefined, "yes", "no"]);
 });
