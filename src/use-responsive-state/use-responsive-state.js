e7d238a56723f0a1eb08d86572ceafb1e9e9c5dc
UpperCod <mtrujillo@outlook.cl>
2022-05-24 23:50:05 -0400

    fix use-responsive-state allowing regenerate state

--- src/use-responsive-state/use-responsive-state.js
+++ src/use-responsive-state/use-responsive-state.js
@@ -62,6 +62,8 @@ export function useResponsiveState(part, ...args) {
         };
       });
 
+    setValue(check);
+
     return () => unlisteners.forEach((fn) => fn());
   }, [template.result]);
 
