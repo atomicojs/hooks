f398957bbde92b1183e3d910f2c052cea325dbfd
UpperCod <uppercod.git@gmail.com>
2022-11-09 14:58:21 -0300

    update atomico

--- src/use-ref-values/use-ref-values.js
+++ src/use-ref-values/use-ref-values.js
@@ -1,43 +1,24 @@
-import { useHook } from "atomico";
+import { useEffect, useRef, useLayoutEffect } from "atomico";
 
 /**
  * @template {import("atomico").Ref[]} T
  * @param {(args: Args<T>)=>(void | ()=>any)} callback
  * @param {T} args
- * @param {boolean} [mode] - by default it is executed in the cycle of useLayoutEffect, if true it will be executed in the cycle of useEffect
+ * @param {boolean} mode
  */
 export function useRefValues(callback, args, mode) {
-  const effect = (state, unmounted) => {
-    const { length } = args;
-    let [prevArgs = [], collector] = state;
-    let withCurrent = 0;
-    let withDiff = 0;
-    let nextArgs = [];
+  const { current } = useRef({ args, mode });
 
-    for (let i = 0; i < length; i++) {
-      const { current } = args[i];
-      if (current != null) {
-        withCurrent++;
-      }
-      if (current != prevArgs[i]) {
-        withDiff++;
-      }
-      nextArgs.push(current);
+  (current.mode ? useEffect : useLayoutEffect)(() => {
+    if (
+      args.some(
+        (ref, i) => ref.current && ref.current !== current.args[i]?.current
+      )
+    ) {
+      current.args = args;
+      callback(args.map(({ current }) => current));
     }
-
-    if ((withDiff || unmounted) && collector) {
-      collector();
-      collector = null;
-    }
-
-    if (withDiff && !unmounted && withCurrent === length) {
-      collector = callback(nextArgs);
-    }
-
-    return [nextArgs, collector];
-  };
-
-  useHook((state = []) => state, !mode && effect, mode && effect);
+  });
 }
 
 /**
