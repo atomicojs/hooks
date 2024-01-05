e88faea9b0133fddd8d02dbbb87e3da1dcdfb694
MatiasWorker <matias.trujillo@blue.cl>
2024-01-04 23:14:24 -0300

    modifies the host's limit to an external reference

--- packages/use-drag-resize/src/index.ts
+++ packages/use-drag-resize/src/index.ts
@@ -1,12 +1,12 @@
-import { Ref, useState, useHost, useEffect } from "atomico";
-import { useListener } from "@atomico/use-listener";
 import { useDebounceState } from "@atomico/use-debounce-state";
+import { useListener } from "@atomico/use-listener";
+import { Ref, useEffect, useState } from "atomico";
 
 export function useDragResize(
 	ref: Ref,
+	refParent: Ref,
 	rootState: number[] = [1, 1],
 ): [boolean, number, number] {
-	const host = useHost();
 	const [active, setActive] = useState(false);
 	const [[x, y], setValue] = useDebounceState(1, rootState, "fps");
 
@@ -17,31 +17,31 @@ export function useDragResize(
 
 	useListener(ref, "mousedown", start);
 
-	useListener(host, "mouseup", end);
+	useListener(refParent, "mouseup", end);
 
-	useListener(host, "mouseleave", end);
+	useListener(refParent, "mouseleave", end);
 
 	useListener(ref, "touchstart", start);
 
-	useListener(host, "touchend", end);
+	useListener(refParent, "touchend", end);
 
-	useListener(host, "touchmove", (event: TouchEvent) => {
+	useListener(refParent, "touchmove", (event: TouchEvent) => {
 		const {
 			targetTouches: [touche],
 		} = event;
-		const rect = host.current.getBoundingClientRect();
+		const rect = refParent.current.getBoundingClientRect();
 		const offsetX = touche.pageX - rect.x;
 		const offsetY = touche.pageY - rect.y;
-		const { current } = host;
+		const { current } = refParent;
 		setValue([
 			offsetX / current.clientWidth,
 			offsetY / current.clientHeight,
 		]);
 	});
 
-	useListener(host, "mousemove", (event: MouseEvent) => {
+	useListener(refParent, "mousemove", (event: MouseEvent) => {
 		if (active) {
-			const { current } = host;
+			const { current } = refParent;
 			setValue([
 				event.offsetX / current.clientWidth,
 				event.offsetY / current.clientHeight,
