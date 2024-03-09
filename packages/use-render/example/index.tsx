f0cf71ffefba91a76dd50eb2033bee0e3de8f094
MatiasWorker <matias.trujillo@blue.cl>
2024-03-08 22:46:09 -0300

    Improve rendering

---
+++ packages/use-render/example/index.tsx
@@ -0,0 +1,21 @@
+import { c } from "atomico";
+import { useRender } from "../src";
+
+const MyComponent = c(
+	({ name, checked }) => {
+		useRender(<input type="checkbox" name={name} checked={checked} />);
+		return (
+			<host shadowDom>
+				<slot />
+			</host>
+		);
+	},
+	{
+		props: {
+			name: String,
+			checked: Boolean,
+		},
+	},
+);
+
+customElements.define("my-component", MyComponent);
--- packages/use-render/src/index.tsx
+++ packages/use-render/src/index.tsx
@@ -1,44 +1,21 @@
-import { useHost, useMemo, useEffect, options } from "atomico";
-
-const host = <host />;
-
-const IdUseRender = Symbol.for("Atomico.useRender");
-
-const props = {
-	[IdUseRender]: true,
-};
+import { useHost, useInsertionEffect, render } from "atomico";
 /**
  * Ensures that the render function always
  * receives a tree that starts from the host tag
  */
-function fillHost(vdom?: any) {
-	if (vdom && typeof vdom == "object") {
-		return vdom.type == "host" ? (
-			{
-				...vdom,
-				props: {
-					...props,
-					...vdom.props,
-				},
-			}
-		) : (
-			<host {...props}>{vdom}</host>
-		);
-	}
-	return host;
-}
+const fillHost = (vnode?: any) =>
+	vnode && vnode.type === "host" ? vnode : <host>{vnode}</host>;
+
 /**
  * Generate a second render, this render escapes the current
  * one and is useful for collaborative work between LightDOM and shadowDOM
  */
-export function useRender(callback: () => any, args?: any[]) {
+export function useRender(callback: () => any, args: any[] = []) {
 	const host = useHost();
+
 	host.id = host.id || Symbol();
-	useMemo(
-		() =>
-			!options.ssr && fillHost(callback()).render(host.current, host.id),
-		args,
-	);
-	// Clean nodes in case of recycling
-	useEffect(() => () => fillHost().render(host.current, host.id), []);
+
+	useInsertionEffect(() => {
+		render(fillHost(callback), host.current, host.id);
+	}, args);
 }
